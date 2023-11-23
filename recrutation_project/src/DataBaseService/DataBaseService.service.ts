3dd2b45f1b324035335b6ce76b908e85d2714ee9
Alkaratus <kubama3ur@wp.pl>
2023-11-23 09:27:26 +0100

    Bugfix in createNewTest function
    I fixed bug which caused exception during
    creation of new test.

--- recrutation_project/src/DataBaseService/DataBaseService.service.ts
+++ recrutation_project/src/DataBaseService/DataBaseService.service.ts
@@ -1,77 +1,89 @@
-import {Injectable} from "@nestjs/common";
-import {InjectRepository} from "@nestjs/typeorm";
-import {Repository} from "typeorm";
-import {Test as TestEntity} from "../DataBaseEntities/Test";
-import {GraphQLInputToEntityConverter} from "../Converters/GraphQLInputToEntityConverter";
-import {NewTest} from "../GraphQLSchemas/NewTest/NewTest";
-import {ChoiceQuestion as ChoiceQuestionEntity} from "../DataBaseEntities/ChoiceQuestion";
-import {MultipleChoiceQuestion as MultipleChoiceQuestionEntity} from "../DataBaseEntities/MultipleChoiceQuestion";
-import {SingleChoiceQuestion as SingleChoiceQuestionEntity} from "../DataBaseEntities/SingleChoiceQuestion";
-
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Test as TestEntity } from '../DataBaseEntities/Test';
+import { GraphQLInputToEntityConverter } from '../Converters/GraphQLInputToEntityConverter';
+import { NewTest } from '../GraphQLSchemas/NewTest/NewTest';
+import { ChoiceQuestion as ChoiceQuestionEntity } from '../DataBaseEntities/ChoiceQuestion';
+import { MultipleChoiceQuestion as MultipleChoiceQuestionEntity } from '../DataBaseEntities/MultipleChoiceQuestion';
+import { SingleChoiceQuestion as SingleChoiceQuestionEntity } from '../DataBaseEntities/SingleChoiceQuestion';
 
 @Injectable()
-export class DataBaseServiceService{
-    constructor( @InjectRepository(TestEntity)private readonly testsRepository: Repository<TestEntity>){}
+export class DataBaseServiceService {
+  constructor(
+    @InjectRepository(TestEntity)
+    private readonly testsRepository: Repository<TestEntity>,
+  ) {}
 
-    async getAllTests():Promise<TestEntity[]>{
-        let tests=await this.testsRepository.find({
-            relations: {
-                choiceQuestions:{
-                    answers:true
-                },
-                orderQuestions:{
-                    answers:true
-                },
-                textQuestions:{
-                    answers:true
-                }
-            },
-        });
-        tests.forEach((test)=>{
-            this.resolveQuestions(test.choiceQuestions);
-        })
-        return tests;
+  async getAllTests(): Promise<TestEntity[]> {
+    const tests = await this.testsRepository.find({
+      relations: {
+        choiceQuestions: {
+          answers: true,
+        },
+        orderQuestions: {
+          answers: true,
+        },
+        textQuestions: {
+          answers: true,
+        },
+      },
+    });
+    tests.forEach((test) => {
+      this.resolveQuestions(test.choiceQuestions);
+    });
+    return tests;
+  }
+
+  async getTestById(id: number) {
+    const test = await this.testsRepository.findOne({
+      relations: {
+        choiceQuestions: {
+          answers: true,
+        },
+        orderQuestions: {
+          answers: true,
+        },
+        textQuestions: {
+          answers: true,
+        },
+      },
+      where: {
+        id: id,
+      },
+    });
+    if (test != null) {
+      this.resolveQuestions(test.choiceQuestions);
     }
+    return test;
+  }
 
-    async getTestById(id:number){
-        let test=await this.testsRepository.findOne({
-            relations: {
-                choiceQuestions:{
-                    answers:true
-                },
-                orderQuestions:{
-                    answers:true
-                },
-                textQuestions:{
-                    answers:true
-                }
-            },
-            where:{
-                id:id
-            }
-        });
-        if(test!=null){
-            this.resolveQuestions(test.choiceQuestions);
-        }
-        return test;
-    }
+  async addNewTest(newTest: NewTest): Promise<TestEntity> {
+    const converter: GraphQLInputToEntityConverter =
+      new GraphQLInputToEntityConverter();
+    const createdTest: TestEntity = this.testsRepository.create(
+      converter.convertTest(newTest),
+    );
+    await this.testsRepository.save(createdTest);
+    this.resolveQuestions(createdTest.choiceQuestions);
+    return createdTest;
+  }
 
-    async addNewTest(newTest: NewTest): Promise<TestEntity> {
-        let converter: GraphQLInputToEntityConverter= new GraphQLInputToEntityConverter();
-        let createdTest: TestEntity = this.testsRepository.create(converter.convertTest(newTest));
-        await this.testsRepository.save(createdTest);
-        return createdTest;
-    }
-
-    resolveQuestions(choiceQuestions: ChoiceQuestionEntity[]):void{
-        choiceQuestions.forEach((question,index,questions)=>{
-            if(question.multiple){
-                questions[index]=new MultipleChoiceQuestionEntity(question.id,question.content,question.answers)
-            }
-            else{
-                questions[index]=new SingleChoiceQuestionEntity(question.id,question.content,question.answers)
-            }
-        })
-    }
-
+  resolveQuestions(choiceQuestions: ChoiceQuestionEntity[]): void {
+    choiceQuestions.forEach((question, index, questions) => {
+      if (question.multiple) {
+        questions[index] = new MultipleChoiceQuestionEntity(
+          question.id,
+          question.content,
+          question.answers,
+        );
+      } else {
+        questions[index] = new SingleChoiceQuestionEntity(
+          question.id,
+          question.content,
+          question.answers,
+        );
+      }
+    });
+  }
 }
